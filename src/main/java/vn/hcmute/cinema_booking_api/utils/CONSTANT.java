eb7e9ef33b7b0ed9f5090f6621585fa219baef51
nguyendk <nd2004lk13@gmail.com>
2026-06-12 21:57:41 +0700

    feat: add constant config

---
+++ src/main/java/vn/hcmute/cinema_booking_api/utils/CONSTANT.java
@@ -0,0 +1,14 @@
+package vn.hcmute.cinema_booking_api.utils;
+
+import java.time.Duration;
+
+public class CONSTANT {
+    public static final Duration HOLD_TICKET_DURATION = Duration.ofMinutes(5);
+
+    public static final String DEFAULT_AVATAR = "https://res.cloudinary.com/demec8nev/image/upload/v1745039879/default_avatar_r7xkiv.png";
+
+    // Mail config
+    public static final String OTP_PREFIX = "OTP:";
+    public static final int OTP_LENGTH = 6;
+    public static final long OTP_EXPIRE_MINUTES = 5;
+}
--- src/main/java/vn/hcmute/cinema_booking_api/utils/Constant.java
+++
@@ -1,8 +0,0 @@
-package vn.hcmute.cinema_booking_api.utils;
-
-import java.time.Duration;
-
-public class Constant {
-    public static final long OTP_EXPIRE_TIME = 5;  //OTP luu 5 minutes
-    public static final Duration HOLD_TICKET_DURATION = Duration.ofMinutes(5);
-}
