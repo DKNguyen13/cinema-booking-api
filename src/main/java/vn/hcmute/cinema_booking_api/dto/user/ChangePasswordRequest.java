1ced58e9b9d2161ae1abe622c295c05875de12d2
nguyendk <nd2004lk13@gmail.com>
2026-06-13 17:31:20 +0700

    feat: add change password request dto

---
+++ src/main/java/vn/hcmute/cinema_booking_api/dto/user/ChangePasswordRequest.java
@@ -0,0 +1,22 @@
+package vn.hcmute.cinema_booking_api.dto.user;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ChangePasswordRequest {
+    @NotBlank(message = "Password must not be blank")
+    @Size(min = 8, max = 50, message = "Password must be between 8 and 50 characters")
+    private String oldPassword;
+
+    @NotBlank(message = "Password must not be blank")
+    @Size(min = 8, max = 50, message = "Password must be between 8 and 50 characters")
+    private String newPassword;
+
+    @NotBlank(message = "Password must not be blank")
+    @Size(min = 8, max = 50, message = "Password must be between 8 and 50 characters")
+    private String confirmPassword;
+}
