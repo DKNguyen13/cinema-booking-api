1b804493dfe36a880a8e44b9b4c322b6dd88664e
nguyendk <nd2004lk13@gmail.com>
2026-06-14 10:56:31 +0700

    feat: add admin service

--- src/main/java/vn/hcmute/cinema_booking_api/services/Impl/AdminService.java
+++ src/main/java/vn/hcmute/cinema_booking_api/services/Impl/AdminService.java
@@ -2,7 +2,13 @@ package vn.hcmute.cinema_booking_api.services.Impl;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import vn.hcmute.cinema_booking_api.dto.admin.UserDetailResponse;
+import vn.hcmute.cinema_booking_api.dto.admin.UserListResponse;
+import vn.hcmute.cinema_booking_api.entity.User;
+import vn.hcmute.cinema_booking_api.exception.BadRequestException;
 import vn.hcmute.cinema_booking_api.repository.UserRepository;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -10,4 +16,51 @@ public class AdminService {
     private final UserRepository userRepository;
 
     // Get all user
+    public List<UserListResponse> getAllUsers() {
+        List<User> users = userRepository.findAll();
+        return users.stream()
+                .map(this::mappingUserToUserList)
+                .toList();
+    }
+
+    // Get user detail
+    public UserDetailResponse getUserDetail(Long userId) {
+        User user = userRepository.findByUserId(userId)
+                .orElseThrow(() -> new BadRequestException("User not found"));
+        return mappingUserToUserDetail(user);
+    }
+
+    // Set active/inactive account
+    public void setStatusUser(Long userId) {
+        User user = userRepository.findByUserId(userId)
+                .orElseThrow(() -> new BadRequestException("User not found"));
+        boolean newStatus = !Boolean.TRUE.equals(user.getIsActive());
+        user.setIsActive(newStatus);
+        userRepository.save(user);
+    }
+
+    //Helper
+    private UserListResponse mappingUserToUserList(User user) {
+        return UserListResponse.builder()
+                .id(user.getUserId())
+                .fullName(user.getFullName())
+                .email(user.getEmail())
+                .phone(user.getPhone())
+                .isActive(user.getIsActive())
+                .build();
+    }
+
+    private UserDetailResponse mappingUserToUserDetail(User user) {
+        return UserDetailResponse.builder()
+                .id(user.getUserId())
+                .fullName(user.getFullName())
+                .email(user.getEmail())
+                .phone(user.getPhone())
+                .address(user.getAddress())
+                .point(user.getPoint())
+                .imageUrl(user.getImageUrl())
+                .isActive(user.getIsActive())
+                .roleName(user.getRole().getRoleName())
+                .build();
+    }
 }
