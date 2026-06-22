d312e091d17de4892c5f1e833ab552ced2f66774
nguyendk <nd2004lk13@gmail.com>
2026-06-22 20:02:38 +0700

    feat: add movie repository

---
+++ src/main/java/vn/hcmute/cinema_booking_api/repositories/MovieRepository.java
@@ -0,0 +1,24 @@
+package vn.hcmute.cinema_booking_api.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import vn.hcmute.cinema_booking_api.entity.Movie;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+    List<Movie> findByIsActiveTrueOrderByReleaseDateDesc();
+    Optional<Movie> findByMovieIdAndIsActiveTrue(Long movieId);
+    List<Movie> findByTitleContainingIgnoreCaseAndIsActiveTrueOrderByReleaseDateDesc(String keyword);
+
+    @Query("""
+        SELECT DISTINCT m
+        FROM Movie m
+        JOIN m.categories c
+        WHERE c.categoryId = :categoryId AND m.isActive = true
+        ORDER BY m.releaseDate DESC
+    """)
+    List<Movie> findActiveMoviesByCategoryId(Long categoryId);
+}
