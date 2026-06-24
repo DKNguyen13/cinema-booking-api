7baf0d7572444d61041fea6cf7351f0b39753900
nguyendk <nd2004lk13@gmail.com>
2026-06-24 11:41:46 +0700

    fix: fix bug get email and role when login

--- src/main/java/vn/hcmute/cinema_booking_api/repositories/UserRepository.java
+++ src/main/java/vn/hcmute/cinema_booking_api/repositories/UserRepository.java
@@ -1,6 +1,8 @@
 package vn.hcmute.cinema_booking_api.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import vn.hcmute.cinema_booking_api.entity.User;
 import java.util.Optional;
@@ -9,6 +11,8 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     Optional<User> findByUserId(Long userId);
+    @Query("select u from User u join fetch u.role where u.email = :email")
+    Optional<User> findByEmailWithRole(@Param("email") String email);
     boolean existsByEmail(String email);
     boolean existsByPhone(String phone);
     boolean existsByEmailOrPhone(String email, String phone);
